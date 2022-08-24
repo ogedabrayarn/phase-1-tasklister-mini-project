370829c43e99b394142a41b85992bd83e47ca46c
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-24 16:34:47 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   document.querySelector('form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     addingEventListener(e.target.new_todo.value)
-//   })
-// });
-// function addingEventListener(todo){
-//   console.log(todo)
-// }
+document.addEventListener("DOMContentLoaded", () => {
+  // your code here
+  let newTaskForm = document.getElementById('create-task-form')
+  let taskList = document.getElementById('list')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let newTask = document.getElementById('new-task-description').value
+    //creating appending element
+    let li = document.createElement('li')
+    li.innerText = task
+    let btn = document.createElement('button')
+    btn.innerText = 'New Value'
 
-  const taskList = new TaskList();
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
+    btn.addEventListener('click', (event) => {
+      let secondTask = prompt('Edit Task')
+      li.innerText = editTask
+    })
+    li.appendChild(editButton)
+    let deleteBtn = document.createElement('button')
+    delBtn.innerText = 'clear'
+    delBtn.addEventListener('click', (event) => {
+      e.target.parentElement.remove()
+    })
+    li.appendChild(delBtn)
 
-  //ul where new tasks will live on the DOM
-  const taskToDo = document.getElementById("tasks");
 
-  const handleToDo = () => (taskToDo.innerHTML = taskList.renderTasks());
-  //attach event listeners
-
-  newTaskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    handleToDo();
-  });
-
-  taskToDo.addEventListener("click", (e) => {
-    if (e.target.nodeName === "btn") {
-      taskList.deleteTask(e.target.dataset.description);
-      handleToDo();
-    }
-  }
-);
+    list.appendChild(li)
+    form.reset()
+  })
+});
